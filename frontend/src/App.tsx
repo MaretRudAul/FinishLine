18729ec040441c7a254c3deb6751c5db1e4e2daa
George Padron <georgenpadron@gmail.com>
2024-09-28 21:36:02 -0500

    Set up api calls and refactored types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import EventCard from './components/EventCard';
+import { EventCardData } from './types';
 
 function App() {
-  const mockupData = {
-    title: "Title",
-    host: "Host",
-    location: "Place",
-    description: "Lorem Ipsum",
-    datetime: new Date(),
-    link: "https://google.com",
-    websiteOrigin: 'Sugma',
-    tag: ['tag1', 'tag2'],
-    pictureUrl: "https://static.campuslabsengage.com/discovery/images/events/social.jpg"
-  };
-  const apiData = [mockupData, mockupData, mockupData,mockupData, mockupData, mockupData,mockupData, mockupData, mockupData,mockupData, mockupData, mockupData];
+  // const mockupData = {
+  //   title: "Title",
+  //   host: "Host",
+  //   location: "Place",
+  //   description: "Lorem Ipsum",
+  //   datetime: new Date(),
+  //   link: "https://google.com",
+  //   websiteOrigin: 'Sugma',
+  //   tag: ['tag1', 'tag2'],
+  //   pictureUrl: "https://static.campuslabsengage.com/discovery/images/events/social.jpg"
+  // };
+  // const apiData = [mockupData, mockupData, mockupData,mockupData, mockupData, mockupData,mockupData, mockupData, mockupData,mockupData, mockupData, mockupData];
+  //
+  const [events, setEvents] = useState<EventCardData[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/fetch-events')
+      .then(response => response.json())
+      .then(data => setEvents(data));
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="flex justify-center items-center flex-wrap gap-4 mt-5">
-      {apiData.map((data) => (
-        <EventCard
-          title={data.title}
-          host={data.host}
-          location={data.location}
-          description={data.description}
-          datetime={data.datetime}
-          link={data.link}
-          websiteOrigin={data.websiteOrigin}
-          tag={data.tag}
-          pictureUrl={data.pictureUrl}
-        />
-      )
-      )}
+        {events.map((data) => (
+          <EventCard
+            title={data.title}
+            host={data.host}
+            location={data.location}
+            description={data.description}
+            datetime={data.datetime}
+            link={data.link}
+            websiteOrigin={data.websiteOrigin}
+            tag={data.tag}
+            pictureUrl={data.pictureUrl}
+          />
+        )
+        )}
       </div>
     </div>
   );
--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -1,87 +1,77 @@
 import React from "react"
+import { EventCardData } from "../types";
 
-interface HeaderProps {
-  title: string;
-  host: string;
-  location: string;
-  description: string;
-  datetime: Date;
-  pictureUrl: string;
-  tag: string[];
-  websiteOrigin: string;
-  link: string;
-}
 
-const EventCard: React.FC<HeaderProps> = ({
+const EventCard: React.FC<EventCardData> = ({
   title, host, location, description, datetime, pictureUrl, tag, websiteOrigin, link
 }) => {
   return (
-    <a 
-    href={link}
-    
-    className="flex items-center justify-center 
+    <a
+      href={link}
+
+      className="flex items-center justify-center 
     h-full w-full sm:w-80 md:w-96 lg:w-1/3 xl:w-1/4 p-4 sm:p-6 md:p-8 text-lg sm:text-xl md:text-2xl gap-4 mb-4
     transform transition-transform duration-300 hover:scale-105
-    border border-gray-300 bg-red-100 rounded-md flex wrap:wrap ">
+    border border-gray-300 bg-red-100 rounded-md wrap:wrap ">
       <div>
-        
-         {/* Title */}
-      <div className="mb-2">
-        <h2 className="text-2xl font-bold text-gray-800 text-center ml-4 mr-4">{title}</h2>
-      </div>
-      
-            {/* Picture */}
-      <div className="w-full h-48 object-cover rounded-md mb-12">
-        <img src={pictureUrl}/>
-      </div>
 
-          {/* Description */}
-      <div className="mb-2">
-        <p className="text-gray-700 ml-4 mr-4">{description}</p>
-      </div>
+        {/* Title */}
+        <div className="mb-2">
+          <h2 className="text-2xl font-bold text-gray-800 text-center ml-4 mr-4">{title}</h2>
+        </div>
 
-      {/* Host */}
-      <div className="mb-2">
-        <p className="text-gray-700 ml-4 mr-4">
-          <strong></strong> {host}
-        </p>
-      </div>
+        {/* Picture */}
+        <div className="w-full h-48 object-cover rounded-md mb-12">
+          <img src={pictureUrl} />
+        </div>
 
-      {/* Location */}
-      <div className="mb-2">
-        <p className="text-gray-700 ml-4 mr-4">
-          <strong></strong> {location}
-        </p>
-      </div>
+        {/* Description */}
+        <div className="mb-2">
+          <p className="text-gray-700 ml-4 mr-4">{description}</p>
+        </div>
 
-      {/* Date and Time */}
-      <div className="mb-2">
-        <p className="text-gray-700 ml-4 mr-4 sm:text-sm">
-          <strong></strong> {datetime.toDateString()}, {datetime.toLocaleTimeString()}
-        </p>
-      </div>
+        {/* Host */}
+        <div className="mb-2">
+          <p className="text-gray-700 ml-4 mr-4">
+            <strong></strong> {host}
+          </p>
+        </div>
 
-      {/* Website Origin 
+        {/* Location */}
+        <div className="mb-2">
+          <p className="text-gray-700 ml-4 mr-4">
+            <strong></strong> {location}
+          </p>
+        </div>
+
+        {/* Date and Time */}
+        <div className="mb-2">
+          <p className="text-gray-700 ml-4 mr-4 sm:text-sm">
+            <strong></strong> {datetime.toDateString()}, {datetime.toLocaleTimeString()}
+          </p>
+        </div>
+
+        {/* Website Origin 
       <div className="mb-2">
         <p className="text-gray-700 ml-4 mr-4">
           <strong>Website:</strong> {websiteOrigin}
         </p>
       </div>  */}
 
-      {/* Tags */}
-      {tag.length > 0 && (
-        <div className="mt-4 flex flex-wrap gap-2 ml-4 mr-4">
-          {tag.map((item, index) => (
-            <span
-              key={index}
-              className="inline-block bg-gray-200 text-gray-700 text-sm px-3 py-1 rounded-full"
-            >
-              {item}
-            </span>
-          ))}
-        </div>
-      )}
-        </div>
+        {/* Tags */}
+        {tag.length > 0 && (
+          <div className="mt-4 flex flex-wrap gap-2 ml-4 mr-4">
+            {tag.map((item, index) => (
+              <span
+                key={index}
+                className="inline-block bg-gray-200 text-gray-700 text-sm px-3 py-1 rounded-full"
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
     </a>
   )
 
